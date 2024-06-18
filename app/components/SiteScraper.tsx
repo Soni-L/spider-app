25af7fa0d051f22fe7bfdb7f8569678aab38dd46
soni <acdsoni@gmail.com>
2024-06-18 13:15:23 +0200

    get a site scraper component going

---
+++ app/components/SiteScraper.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import Dialog from "@mui/material/Dialog";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import CloseIcon from "@mui/icons-material/Close";
+import Slide from "@mui/material/Slide";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function SiteScraper({ open, url, handleClose }) {
+  return (
+    <Dialog
+      fullScreen
+      open={open}
+      onClose={handleClose}
+      TransitionComponent={Transition}
+    >
+      <AppBar sx={{ position: "relative" }}>
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            onClick={handleClose}
+            aria-label="close"
+          >
+            <CloseIcon />
+          </IconButton>
+          <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+            Scraper
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <iframe
+        name={"iframe"}
+        src={url}
+        style={{ height: "200px", width: "200px" }}
+      ></iframe>
+    </Dialog>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,53 @@
 "use client";
+
+import { TextField, Button } from "@mui/material";
 import { useRef, useState } from "react";
+import SiteScraper from "./components/SiteScraper";
+
+type SiteViewModalProps = {
+  open: boolean;
+  url: string;
+};
 
 export default function Home() {
   const inputUrl = useRef<HTMLInputElement | null>(null);
+  const [siteViewModalProps, setSiteViewModalProps] =
+    useState<SiteViewModalProps>({ open: false, url: "" });
 
   return (
     <main>
-      <input ref={inputUrl}></input>
-      <button onClick={() => console.log(inputUrl?.current?.value)}>
-        submit
-      </button>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          maxWidth: "200px",
+          margin: "auto",
+          gap: "10px",
+          padding: "30px 0",
+        }}
+      >
+        <TextField
+          size="small"
+          ref={inputUrl}
+          placeholder="Input url"
+        ></TextField>
+        <Button
+          variant="contained"
+          onClick={() =>
+            setSiteViewModalProps({
+              url: inputUrl?.current?.value || "",
+              open: true,
+            })
+          }
+        >
+          Open Site
+        </Button>
+      </div>
+      <SiteScraper
+        open={siteViewModalProps.open}
+        url={siteViewModalProps.url}
+        handleClose={() => setSiteViewModalProps({ open: false, url: "" })}
+      />
     </main>
   );
 }
